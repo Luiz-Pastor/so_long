9e38eb62c986a210570ab9c74e913908fec4106d
Luiz-Pastor <luigpacr5@gmail.com>
2023-11-07 11:16:16 +0100

    Started functions about movement

---
+++ inc/movement.h
@@ -0,0 +1,12 @@
+#ifndef MOVEMENT_H
+# define MOVEMENT_H
+
+# include "structs.h"
+# include "game.h"
+
+void	move_right(t_game *game);
+void	move_down(t_game *game);
+void	move_left(t_game *game);
+void	move_right(t_game *game);
+
+# endif
---
+++ src/movement.c
@@ -0,0 +1,44 @@
+#include "../inc/map.h"
+
+// TODO
+void	move_right(t_game *game)
+{
+	// Si se sale del mapa, nada
+	if (game->player->coords.x + 1 == game->map->columns)
+		return ;
+	
+	// Si se estampa contra muro, nada
+	if (game->map->map[game->player->coords.y][game->player->coords.x + 1] == '1')
+		return ;
+	
+	// Si va a la puerta sin todos los coleccionables, nada
+	if (game->map->map[game->player->coords.y][game->player->coords.x + 1] == 'E' && \
+		game->player->collec != game->map->collectable)
+		return ;
+	
+	// Si va a la puerta con todos los coleccionables, ALGO
+	if (game->map->map[game->player->coords.y][game->player->coords.x + 1] == 'E' && \
+		game->player->collec == game->map->collectable)
+		return ;
+
+	game->map->map[game->player->coords.y][game->player->coords.x] = '0';
+	if (game->map->map[game->player->coords.y][game->player->coords.x + 1] == 'C')
+		(game->player->collec)++;
+	game->map->map[game->player->coords.y][game->player->coords.x + 1] = 'P';
+	(game->steps)++;
+}
+
+void	move_down(t_game *game)
+{
+
+}
+
+void	move_left(t_game *game)
+{
+
+}
+
+void	move_right(t_game *game)
+{
+
+}
