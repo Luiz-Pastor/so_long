b609ba339460b2184743bc952a2049e9d47fb9e2
Luiz-Pastor <luigpacr5@gmail.com>
2023-11-15 12:51:21 +0100

    Fixed leaks

--- src/screen.c
+++ src/screen.c
@@ -6,7 +6,7 @@
 /*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 08:58:26 by lpastor-          #+#    #+#             */
-/*   Updated: 2023/11/14 10:11:45 by lpastor-         ###   ########.fr       */
+/*   Updated: 2023/11/15 10:42:30 by lpastor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,22 +25,7 @@ int	render(t_data *screen)
 		x = 0;
 		while (screen->map[y][x])
 		{
-			if (screen->map[y][x] == 'P')
-			{
-				mlx_put_image_to_window(screen->mlx, screen->win, screen->floor_sprite, x * IMAGE_WEIGHT, y * IMAGE_HEIGHT);
-				mlx_put_image_to_window(screen->mlx, screen->win, screen->player_sprite, x * IMAGE_WEIGHT, y * IMAGE_HEIGHT);
-			}
-			else if (screen->map[y][x] == '1')
-				mlx_put_image_to_window(screen->mlx, screen->win, screen->wall_sprite, x * IMAGE_WEIGHT, y * IMAGE_HEIGHT);
-			else if (screen->map[y][x] == 'C')
-			{
-				mlx_put_image_to_window(screen->mlx, screen->win, screen->floor_sprite, x * IMAGE_WEIGHT, y * IMAGE_HEIGHT);
-				mlx_put_image_to_window(screen->mlx, screen->win, screen->coll_sprite, x * IMAGE_WEIGHT, y * IMAGE_HEIGHT);
-			}
-			else if (screen->map[y][x] == 'E')
-				mlx_put_image_to_window(screen->mlx, screen->win, screen->end_sprite, x * IMAGE_WEIGHT, y * IMAGE_HEIGHT);
-			else
-				mlx_put_image_to_window(screen->mlx, screen->win, screen->floor_sprite, x * IMAGE_WEIGHT, y * IMAGE_HEIGHT);
+			manage_sprites(screen, x, y);
 			x++;
 		}
 		y++;
@@ -48,9 +33,10 @@ int	render(t_data *screen)
 	return (0);
 }
 
-void	end_game(t_data *screen)
+int	end_game(t_data *screen)
 {
 	mlx_destroy_window(screen->mlx, screen->win);
+	print_end(screen->game);
 	exit(0);
 }
 
@@ -71,37 +57,53 @@ int	key_hook(int key, t_data *screen)
 
 static int	set_sprites(t_data *screen)
 {
-	int	w;
-	int	h;
+	int		w;
+	int		h;
+	void	*mlx;
 
 	w = IMAGE_WEIGHT;
 	h = IMAGE_HEIGHT;
-	screen->player_sprite = mlx_xpm_file_to_image(screen->mlx, PLAYER_INNER1, &w, &h);
+	mlx = screen->mlx;
+	screen->player_sprite = mlx_xpm_file_to_image(mlx, PLAYER_INNER1, &w, &h);
 	if (!screen->player_sprite)
 		return (0);
-	screen->wall_sprite = mlx_xpm_file_to_image(screen->mlx, WALL_INNER1, &w, &h);
+	screen->wall_sprite = mlx_xpm_file_to_image(mlx, WALL_INNER1, &w, &h);
 	if (!screen->player_sprite)
 		return (0);
-	screen->floor_sprite = mlx_xpm_file_to_image(screen->mlx, BACKGROUND_INNER1, &w, &h);
-	screen->end_sprite = mlx_xpm_file_to_image(screen->mlx, DOOR_CLOSE, &w, &h);
-	screen->coll_sprite = mlx_xpm_file_to_image(screen->mlx, EGG, &w, &h);
+	screen->floor_sprite = mlx_xpm_file_to_image(mlx, BACK_INNER1, &w, &h);
+	if (!screen->floor_sprite)
+		return (0);
+	screen->end_sprite = mlx_xpm_file_to_image(mlx, DOOR_CLOSE, &w, &h);
+	if (!screen->end_sprite)
+		return (0);
+	screen->coll_sprite = mlx_xpm_file_to_image(mlx, EGG, &w, &h);
+	if (!screen->coll_sprite)
+		return (0);
 	return (1);
 }
 
 t_data	*get_screen(int columns, int rows)
 {
 	t_data	*screen;
+	int		weight;
+	int		height;
 
+	weight = IMAGE_WEIGHT * columns;
+	height = IMAGE_HEIGHT * rows;
 	screen = (t_data *) malloc(sizeof(t_data));
 	if (!screen)
 		return (NULL);
 	screen->mlx = mlx_init();
-	screen->win = mlx_new_window(screen->mlx, IMAGE_WEIGHT * columns, IMAGE_HEIGHT * rows, "so long!");
+	screen->win = mlx_new_window(screen->mlx, weight, height, "so long!");
 	if (!set_sprites(screen))
 	{
+		exit(1);
+		mlx_destroy_window(screen->mlx, screen->win);
 		free(screen);
 		return (NULL);
 	}
 	mlx_key_hook(screen->win, key_hook, screen);
+	mlx_loop_hook(screen->mlx, render, screen);
+	mlx_hook(screen->win, 17, 0, end_game, screen);
 	return (screen);
 }
